c730d2a487c5c74b4cc3b07a06a94e963fd264ba
Arthuchaut <arthur.audebrand@gmail.com>
2023-09-07 00:59:32 +0200

    Added alert management

--- Modules/Core.lua
+++ Modules/Core.lua
@@ -3,21 +3,68 @@ Core.__index = Core
 
 function Core.New()
     local self = setmetatable({}, Core)
+    self.playerPosition = nil
+    self.targetPosition = nil
+    self.playerDistanceFromTarget = nil
+    self.playerBearing = nil
+    self.distanceAlertEnabled = false
+    self.instanceAlertEnabled = false
     return self
+end
+
+function Core:EnableDistanceAlertMessage()
+    print("Distance alert enabled!", self.playerDistanceFromTarget)
+end
+
+function Core:UpdateBearingInfo()
+    print("Bearing:", self.playerBearing)
+end
+
+function Core:DisableDistanceAlertMessage()
+    print("Distance alert disabled.")
+end
+
+function Core:EnableInstanceAlertMessage()
+    print("Instance alert inabled!")
+end
+
+function Core:DisableInstanceAlertMessage()
+    print("Instance alert disabled.")
 end
 
 function Core:UpdateFrame()
     if StayCloseSettings.variable.distanceWatcherEnabled then
-        local playerPosition = Localization:GetWorldPosition("player")
+        self.playerPosition = Localization:GetWorldPosition("player")
 
         if UnitExists(StayCloseSettings.variable.targetID) then
-            -- local targetPosition = Localization:GetWorldPosition(StayCloseSettings.variable.targetID)
-            local targetPosition = Position.New(-8825, 634) -- The place near the auction house in Stomwind
-            local distance = Localization:GetEuclideanDistance(playerPosition, targetPosition)
+            -- self.targetPosition = Localization:GetWorldPosition(StayCloseSettings.variable.targetID)
+            self.targetPosition = Position.New(-8825, 634, 0) -- The place near the auction house in Stomwind
 
-            if distance >= StayCloseSettings.variable.minSafetyDistance then
-                local bearing = Localization:GetRelativeBearing(playerPosition, targetPosition)
-                print("Careful!", distance, bearing)
+            if self.playerPosition.mapID ~= self.targetPosition.mapID then
+                if not self.instanceAlertEnabled then
+                    self.instanceAlertEnabled = true
+                    self:EnableInstanceAlertMessage()
+                end
+            else
+                if self.instanceAlertEnabled then
+                    self.instanceAlertEnabled = false
+                    self:DisableInstanceAlertMessage()
+                end
+
+                self.playerDistanceFromTarget = Localization:GetEuclideanDistance(self.playerPosition, self.targetPosition)
+
+                if self.playerDistanceFromTarget >= StayCloseSettings.variable.minSafetyDistance then
+                    self.playerBearing = Localization:GetRelativeBearing(self.playerPosition, self.targetPosition)
+                    self:UpdateBearingInfo()
+
+                    if not self.distanceAlertEnabled then
+                        self.distanceAlertEnabled = true
+                        self:EnableDistanceAlertMessage()
+                    end
+                elseif self.distanceAlertEnabled then
+                    self.distanceAlertEnabled = false
+                    self:DisableDistanceAlertMessage()
+                end
             end
         end
     end
